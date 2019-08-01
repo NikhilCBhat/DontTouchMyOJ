77c8a160e4b8487cca9570815ff941987b7ca96a
Nikhil Bhat <bhatnikhil314@gmail.com>
2019-08-01 10:58:08 -0900

    Added encodings compiled on pi

--- encodings/generateEncodings.py
+++ encodings/generateEncodings.py
@@ -2,13 +2,15 @@ from os import listdir
 import face_recognition
 import numpy as np
 
-for index, filePath in enumerate(listdir(".")):
+i = 0
+for filePath in listdir("."):
 
     if any(filePath[-3:].lower() == extension for extension in ["png", "jpg"]):
-        
+
         imageFile = face_recognition.load_image_file(filePath)
         try:
             encoding = face_recognition.face_encodings(imageFile)[0]
-            np.savetxt("encoding"+str(index)+".csv", encoding, delimiter=",")
+            np.savetxt("encoding"+str(i)+".csv", encoding, delimiter=",")
+            i += 1
         except:
             pass

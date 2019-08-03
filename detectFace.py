cdebdcf2230a3ca6e9e6354507dd2bf85d05f7f5
Nikhil Bhat <bhatnikhil314@gmail.com>
2019-08-03 09:33:47 -0900

    Adding face detection file

---
+++ detectFace.py
@@ -0,0 +1,19 @@
+import face_recognition
+import numpy as np
+
+numEncodings = 4
+allEncodings = [np.loadtxt("encodings/encoding"+str(i)+".csv") for i in range(numEncodings)]
+
+def sameFace(image=None, filePath="image.jpg"):
+	if image is None:
+		image = face_recognition.load_image_file(filePath)
+	print("Loaded Image")
+	try:
+		newEncoding = face_recognition.face_encodings(image)[0]
+	except:
+		return [False]
+	print("Got new encoding")
+	return face_recognition.compare_faces(allEncodings, newEncoding)
+
+
+

ad6127daf3be9ebb4de02b1d640ec67aaf493240
nikhilcbhat <bhatnikhil314@gmail.com>
2019-08-05 13:29:20 -0400

    Changed return when no faces to an empty list

--- detectFace.py
+++ detectFace.py
@@ -4,14 +4,14 @@ import numpy as np
 numEncodings = 4
 allEncodings = [np.loadtxt("encodings/encoding"+str(i)+".csv") for i in range(numEncodings)]
 
-def sameFace(image=None, filePath="image.jpg"):
+def sameFace(image=None, filePath=""):
 	if image is None:
 		image = face_recognition.load_image_file(filePath)
 	print("Loaded Image")
 	try:
 		newEncoding = face_recognition.face_encodings(image)[0]
 	except:
-		return [False]
+		return []
 	print("Got new encoding")
 	return face_recognition.compare_faces(allEncodings, newEncoding)
 
